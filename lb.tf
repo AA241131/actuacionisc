5a80e03a9ce0a640783d75f42463a6bfab1f4ebd
Andres <AA241131@fi365.ort.edu.uy>
2026-05-07 00:33:34 -0300

    names

--- lb.tf
+++ lb.tf
@@ -17,7 +17,7 @@ resource "aws_lb" "ac2-lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ac2-lb-sg.id]
-  subnets            = [aws_subnet.ac1-public-subnet-1.id, aws_subnet.ac1-private-subnet-2.id]
+  subnets            = [aws_subnet.ac2-public-subnet-1.id, aws_subnet.ac2-public-subnet-2.id]
 
   enable_deletion_protection = false
 
